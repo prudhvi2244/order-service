3ca4eeec4f464ffd0811d9d5fbea0624d776123f
Mahadri <raj.rajeev2244@gmail.com>
2024-03-07 15:35:24 +0530

    first commit

--- src/main/java/com/order/in/service/PaymentClient.java
+++ src/main/java/com/order/in/service/PaymentClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "PAYMENT-SERVICE",url="http://13.126.4.93:9092")
+@FeignClient(name = "PAYMENT-SERVICE",url = "http://34.213.27.246:9092")
 public interface PaymentClient {
 
     @PostMapping(value = "/payment/makePayment")
