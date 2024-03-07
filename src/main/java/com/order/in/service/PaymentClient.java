fc21952ce02f8c80b89fd186e3901b06cfa5f584
Mahadri <raj.rajeev2244@gmail.com>
2024-03-07 11:55:55 +0530

    first commit

--- src/main/java/com/order/in/service/PaymentClient.java
+++ src/main/java/com/order/in/service/PaymentClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "payment-service-container")
+@FeignClient(name = "PAYMENT-SERVICE")
 public interface PaymentClient {
 
     @PostMapping(value = "/payment/makePayment")
